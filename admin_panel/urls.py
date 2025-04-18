af7d1a5d619988ba9d9a6550566de3437ea1f1e1
Egor <egorokt@inbox.ru>
2025-04-18 22:19:09 +0300

    added test version deploy node

--- admin_panel/urls.py
+++ admin_panel/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('dashboard/', views.dashboard, name='dashboard'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('nodes/', views.deploy, name='deploy')
 ]
--- admin_panel/views.py
+++ admin_panel/views.py
@@ -1,7 +1,35 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from .forms import DeployForm
+from fabric import Connection
 
 
 @login_required
 def dashboard(request):
     return render(request, 'admin_panel/dashboard.html')
+
+def deploy(request):
+    if request.method == 'POST':
+        form = DeployForm(request.POST)
+        if form.is_valid():
+            REPOSITORY_URL = "https://github.com/ByteForge-Systems/vpn-node.git"
+            REPOSITORY_BRANCH = "dev"
+            REPOSITORY_FOLDER = "vpn-node"
+            host = '104.164.54.91'
+            user = 'root'
+            password = 'Eds2uqdVUZSP'
+            try:
+                conn = Connection(host=host, user=user, connect_kwargs={"password": password})
+                command = (
+                    f"apt update && apt upgrade -y && apt install git -y &&"
+                    f"git clone --branch {REPOSITORY_BRANCH} {REPOSITORY_URL} && "
+                    f"cd {REPOSITORY_FOLDER} && chmod +x install.sh && ./install.sh"
+                )
+                result = conn.sudo(command, pty=True, hide=True)
+                print(f"Success on {host}: {result.stdout}")
+            except Exception as e:
+                print(f"Error on {host}: {str(e)}")   
+    else:
+        form = DeployForm()
+
+    return render(request, 'admin_panel/deploy.html')
