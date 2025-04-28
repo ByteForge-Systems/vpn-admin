5bc020f632ab7ae9d8dba5eb31daf266a2a4f298
amboy <savenkovanton12@gmail.com>
2025-04-28 19:34:39 +0300

    Feature/design (#3)
    * add static and auth template
    
    * add dashboard
    
    * add servers
    
    * add servers part 2
    
    * add servers.html

--- admin_panel/urls.py
+++ admin_panel/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('', views.dashboard, name='dashboard'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('servers/', views.servers, name='servers')
 ]
--- admin_panel/views.py
+++ admin_panel/views.py
@@ -1,3 +1,4 @@
+import requests
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
@@ -5,3 +6,18 @@ from django.shortcuts import render
 @login_required
 def dashboard(request):
     return render(request, 'admin_panel/dashboard.html')
+
+# TODO: сделать кнопку включения
+# TODO: отредактировать дополнительную информацию на кнопке
+# TODO: ?добавить страничку ноды?
+@login_required
+def servers(request):
+    api_url = "http://46.254.17.174:8080/api/nodes"
+    try:
+        response = requests.get(api_url)
+        nodes = response.json()
+    except requests.exceptions.RequestException as e: #тут надо перепроверить
+        nodes = []
+
+    return render(request, 'admin_panel/servers.html', {'nodes': nodes})
+
