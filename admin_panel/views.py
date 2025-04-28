1fd1599802c26374e15708e7a4325df9e27d463d
anton <savenkovanton12@gmail.com>
2025-04-28 11:17:12 +0300

    add servers

--- admin_panel/views.py
+++ admin_panel/views.py
@@ -1,7 +1,14 @@
+import requests
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
-
+#TODO
+#create views api/nodes
 @login_required
 def dashboard(request):
     return render(request, 'admin_panel/dashboard.html')
+
+@login_required
+def servers(request):
+    return render(request, 'admin_panel/servers.html')
+
