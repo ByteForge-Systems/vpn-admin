3e9ec3c9374e343d28f0c24ec20bed30e15825ab
anton <savenkovanton12@gmail.com>
2025-04-28 19:19:48 +0300

    add servers part 2

--- admin_panel/views.py
+++ admin_panel/views.py
@@ -2,13 +2,22 @@ import requests
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
-#TODO
-#create views api/nodes
+
 @login_required
 def dashboard(request):
     return render(request, 'admin_panel/dashboard.html')
 
+# TODO: сделать кнопку включения
+# TODO: отредактировать дополнительную информацию на кнопке
+# TODO: ?добавить страничку ноды?
 @login_required
 def servers(request):
-    return render(request, 'admin_panel/servers.html')
+    api_url = "http://46.254.17.174:8080/api/nodes"
+    try:
+        response = requests.get(api_url)
+        nodes = response.json()
+    except requests.exceptions.RequestException as e: #тут надо перепроверить
+        nodes = []
+
+    return render(request, 'admin_panel/servers.html', {'nodes': nodes})
 
