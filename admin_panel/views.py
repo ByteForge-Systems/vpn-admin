effcfe394c2760e254ec3bbd86c375065314f372
Egor <egorokt@inbox.ru>
2025-05-29 17:25:41 +0300

    оказывается дело было в том, что другая ссылка на апи была...

--- admin_panel/views.py
+++ admin_panel/views.py
@@ -93,7 +93,7 @@ def deploy(request):
 
 @login_required
 def servers(request):
-    api_url = "http://46.254.17.174:8080/api/nodes"
+    api_url = "http://164.215.97.44:8080/api/nodes"
     try:
         response = requests.get(api_url)
         nodes = response.json()
